99359ba5bdc40f0b2cfa56c19abf47ea9dc05bed
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-07 17:50:52 +0200

    compelated getAge

--- main.js
+++ main.js
@@ -1,21 +1,43 @@
 function getAge() {
   let age = Number(prompt("Введіть свій рік народження"));
 
-  if (age === null) {
+  if (age == false) {
     alert("Шкода, що Ви не захотіли ввести свій рік народження, Бай бай!)");
+    return null;
+  } else if (isNaN(age)) {
+    alert("Потрібно ввести саме рік Вашого народження");
+    return getAge();
+  }
+
+  let currentYear = new Date().getFullYear();
+  let ageUser = currentYear - age;
+  return ageUser;
+}
+
+function getCity() {
+  let city = prompt("Введіть місто, в якому ти живеш");
+  let cityToCheck = city
+    ? Number(city)
+    : alert(
+        "Шкода, що Ви не захотіли ввести свое місто, в якому ти живеш, Бай бай!)"
+      );
+  if (!isNaN(cityToCheck)) {
+    alert("Потрібно ввести саме свое місто, в якому ти живеш");
+    getCity();
   } else {
-    if (isNaN(age)) {
-      alert("Потрібно ввести саме рік Вашого народження");
-      getAge();
-    } else {
-      let currentYear = new Date().getFullYear();
-      let ageUser = currentYear - age;
-      return ageUser;
-    }
+    return city;
   }
 }
 
-getAge();
+function displayInfoUser() {
+  let age = getAge();
+  if (age === null) return;
+
+  let city = getCity();
+  if (city === null) return;
+  alert("Ваш вік: " + age + "\n" + "Ти живеш у столиці " + city + ".");
+}
+displayInfoUser();
 // "Тобі  років"
 // "Ти живеш у столиці "
 // "Ти живеш у місті "
